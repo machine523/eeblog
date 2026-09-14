487ab30c7738f208daf5e718e2c7e6144bbc2477
machine523 <2212473657@qq.com>
2026-09-14 14:20:30 +0800

    update posts

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,7 +11,7 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      <main className="mx-auto w-full max-w-5xl flex-1 px-5">{children}</main>
+      <main className="mx-auto w-full max-w-3xl flex-1 px-5">{children}</main>
       <Footer />
     </div>
   )
